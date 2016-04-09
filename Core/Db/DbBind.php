fdf2a26768909ca831c1919883dedfa94ef6db04
admin <admin@gmail.com>
2016-04-09 12:36:19 +0800

    update

---
+++ Core/Db/DbBind.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Db use bind params values with more safety
+ * 
+ */
+namespace Db\DbBind;
+
+class DbBind{
+	public function __construct(){
+
+	}
+
+	public function _bindParam(){
+
+	}
+
+	public function _bindValue(){
+
+	}
+
+}
